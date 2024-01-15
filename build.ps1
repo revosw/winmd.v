715712a93a9550a4244ae7c9837458c83ba5aa3e
revosw <simenwiik@hotmail.com>
2024-01-15 10:29:42 +0100

    build: include cJSON when compiling

--- build.ps1
+++ build.ps1
@@ -1,1 +1,4 @@
-v -o wip.c -cc msvc .; cl wip.c /link runtimeobject.lib rometadata.lib /debug:full
+# &{Import-Module @"
+# C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll
+# "@; Enter-VsDevShell 913dd44f}; cd /dev/v/winmd; `
+v -o bin/main.c -cc msvc .; cl bin\main.c /Fo"bin\" /Fe"bin\" /Fd"bin\" /MDd -I "C:\dev\_tools\v\thirdparty\cJSON" "C:\dev\_tools\v/thirdparty/cJSON/cJSON.obj" /link runtimeobject.lib rometadata.lib advapi32.lib /debug:full
