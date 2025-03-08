8ca55418b3789bd1583c81e23ade8da2feadf39e
revosw <simenwiik@hotmail.com>
2025-03-08 17:22:59 +0100

    chore: remove unused build.ps1 script

--- build.ps1
+++
@@ -1,4 +0,0 @@
-# &{Import-Module @"
-# C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll
-# "@; Enter-VsDevShell 913dd44f}; cd /dev/v/winmd; `
-v -o bin/main.c -cc msvc .; cl bin\main.c /Fo"bin\" /Fe"bin\" /Fd"bin\" /MDd -I "C:\dev\_tools\v\thirdparty\cJSON" "C:\dev\_tools\v/thirdparty/cJSON/cJSON.obj" /link runtimeobject.lib rometadata.lib advapi32.lib /debug:full
